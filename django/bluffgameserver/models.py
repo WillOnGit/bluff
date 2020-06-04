052715a5552959a7be5f4c746feb046adfda418e
Will Bolton <git@bolton.me.uk>
2020-06-04 16:08:41 +0100

    Create initial models, urls, views, templates and migrations
    A pretty large commit but the resulting app is pretty straightforward.
    The only 'visible' result is an unfinished welcome page and a lobby
    which responds correctly if the person is logged in. All the other work
    is provisional backend stuff.
    
    There are some placeholder methods and attributes on the model which I
    anticipate I'll need to finish later, although I still haven't figured
    out how exactly the client-server communication is going to work (likely
    via websockets) + I'm a little rusty at Django so I may have confused
    some views and methods.
    
    It seems very likely I'll need a few extra models for a working app, but
    I'm not sure what those would be exactly so for now the essential
    remains the only model.

--- django/bluffgameserver/models.py
+++ django/bluffgameserver/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class BluffGame(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, related_name='creator', on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+    #opponent = models.ForeignKey(
+    #    User, related_name='opponent', null=True, blank=True)
+    max_players = models.IntegerField(default=4)
+    active_players = models.IntegerField(default=0)
+
+    date_started = models.DateTimeField()
+    has_started = models.BooleanField(default=False)
+    # initially just support for one round per player
+    current_round = models.IntegerField(default=0)
+    current_turn = models.ForeignKey(User, related_name='current_turn', on_delete=models.PROTECT)
+
+    has_completed = models.BooleanField(default=False)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    winner = models.ForeignKey(User, related_name='winner', null=True, blank=True, on_delete=models.PROTECT)
+
+    # methods
+    def __str__(self):
+        return self.name
+
+    def start_game(self):
+        # set started date + has started, prevent players from joining, start round 1
+        pass
+
+    def new_round(self):
+        # while game not over, increment round counter and implement round
+        pass
+
+    def complete_game(self):
+        # declare winner, set has completed and date complete
+        pass
--- django/bluffgameserver/views.py
+++ django/bluffgameserver/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    return HttpResponse("This will eventually be a quick signup page, simply asking for a name before redirecting to /lobby. Do that manually for now!")
+
+def quick_signup(request):
+    return HttpResponse("yo")
+
+def present_lobby(request):
+    return render(request, 'bluffgameserver/lobby.html')
+
+def create_game(request):
+    return HttpResponse("yo")
+
+def join_game(request):
+    return HttpResponse("yo")
+
+def view_game(request):
     return HttpResponse("yo")
